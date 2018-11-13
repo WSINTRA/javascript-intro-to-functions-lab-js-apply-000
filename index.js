2def53dd687c35c245a5479ad7e485f3f22f48da
William Robert Simpson <will.simpson85@gmail.com>
2018-11-13 22:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function whisper(string){
 function logShout(string){
   console.log(string.toUpperCase())
 }
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if string.toUpperCase {return "YES INDEED!";}
+  else if string.toLowerCase {return "I can't hear you!";}
+  else {return "I love you, too.";}
+    
+  }
