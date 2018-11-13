85ba180dcce26ea106a8009b1ad9fe2068b7479d
William Robert Simpson <will.simpson85@gmail.com>
2018-11-13 22:07:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function sayHiToGrandma(string){
   if string.toUpperCase {return "YES INDEED!";}
   else if string.toLowerCase {return "I can't hear you!";}
   else {return "I love you, too.";}
-    
+
   }
